de5d2531cd065f8df48f01416d92d6595a07e855
Prabhu Bisht <prabhubisht0126@gmail.com>
2024-07-24 09:17:58 +0530

    Day 6 - Partial

---
+++ dSix.js
@@ -0,0 +1,55 @@
+// *** Day 5:- Arrays ***
+//--------------------------------------------------------
+// ### Activity 1:- Array creation & Access
+
+// # Task 1
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+
+// # Task 2
+// console.log(arr[0], arr[4]);
+
+//--------------------------------------------------------
+// ### Activity 2:- Array Methods(Basic)
+
+// # Task 3
+
+let arr = [1, 2, 3, 4, 5];
+// arr.push(23);
+// console.log(arr);
+
+// # Task 4
+
+// arr.pop();
+// console.log(arr);
+
+// # Task 5
+
+// arr.shift();
+// console.log(arr);
+
+// # Task 6
+
+// arr.unshift(6);
+// console.log(arr);
+
+//--------------------------------------------------------
+// ### Activity 3:- Array Methods(Intermediate)
+
+// # Task 7
+
+//--------------------------------------------------------
+// ### Activity 4:- Array Iteration
+
+// # Task 10
+
+// for (let i = 0; i <= arr.length - 1; i++) {
+// 	console.log(arr[i]);
+// }
+
+// #Task 11
+
+array.forEach((element) => {
+	console.log(arr[element]);
+});
